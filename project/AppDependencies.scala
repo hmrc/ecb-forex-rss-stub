0d2568a3ae39d11a23a597ce8d0118aea429a975
Lee Powell <lee.powell@digital.hmrc.gov.uk>
2025-02-19 14:33:21 +0000

    VEOSS-2037 | Update dependencies as per Bobby warnings

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,11 +6,11 @@ import sbt.*
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % "9.5.0"
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % "9.9.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % "9.5.0",
+    "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % "9.9.0",
     "com.vladsch.flexmark"    %  "flexmark-all"               % "0.64.8",
     "org.scalatest"           %% "scalatest"                  % "3.2.19"
   ).map(_ % "test, it")
